896cf71823ef8a7ec9b43b8f15a8c81652caaa42
himanshu_nishad <himanshunishad620@gmail.com>
2026-09-05 11:45:48 +0530

    bug fixed

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -298,7 +298,7 @@ const scanQr = async (req, res) => {
 
     const analyticsFields = {
       totalScans: 1,
-      [`daily.${currentDate}.clicks`]: 1,
+      [`daily.${currentDate}.scans`]: 1,
       [`browser.${browserName}`]: 1,
       [`cities.${cityName}`]: 1,
     };
