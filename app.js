2d710b5faafa678632ff0caa04fc3364b316c107
himanshu_nishad <himanshunishad620@gmail.com>
2026-09-06 08:32:34 +0530

    health api added

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
   res.redirect(process.env.CLIENT_URL);
 });
+app.head("/health", (req, res) => {
+  console.log("Health Checked");
+  res.status(200).end();
+});
 app.use("/", FirstShortUrl);
 app.use("/", LinkRoutes);
 
