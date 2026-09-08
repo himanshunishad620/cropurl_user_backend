ba3b8a46e95615fd9f180f9005d8a4ea0ee1b0b9
himanshu_nishad <himanshunishad620@gmail.com>
2026-09-08 06:53:26 +0530

    test ip api

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ app.head("/health/check", (req, res) => {
   console.log("Health Checked");
   return res.sendStatus(200);
 });
+app.get("/test", (req, res) => {
+  console.log("req.ip:", req.ip);
+  console.log("req.ips:", req.ips);
+  console.log("x-forwarded-for:", req.headers["x-forwarded-for"]);
+  console.log("x-real-ip:", req.headers["x-real-ip"]);
+
+  res.json({
+    ip: req.ip,
+    ips: req.ips,
+    forwardedFor: req.headers["x-forwarded-for"],
+    realIp: req.headers["x-real-ip"],
+  });
+});
+
 app.get("/", (req, res) => {
   res.redirect(process.env.CLIENT_URL);
 });
